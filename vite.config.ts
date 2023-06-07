cdc0f11605469b44dae9d658dd153b90d22b0680
bruno <bruno_am_22@hotmail.com>
2023-06-07 19:53:13 -0300

    Actualizado

--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
             entryFileNames: "bundle.js", // Especifica el nombre del archivo de salida personalizado
          },
          input: {
-            main: "./dist/index.html",
+            main: "./index.html",
          },
       },
    },
