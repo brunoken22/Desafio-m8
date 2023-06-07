b014f9841647183d1efbb025dc555807c9decd33
bruno <bruno_am_22@hotmail.com>
2023-06-07 19:32:17 -0300

    Actualizadi

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
-   plugins: [react()],
+   plugins: [reactRefresh()],
 
    css: {
       modules: {
