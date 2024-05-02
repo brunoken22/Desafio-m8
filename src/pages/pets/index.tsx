21d881396680b263edaab82066383b77066984b5
Bruno Ken <bruno_am_22@hotmail.com>
2024-05-01 22:23:44 -0300

    error reparate

--- src/pages/pets/index.tsx
+++ src/pages/pets/index.tsx
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import {Text} from '../../ui/text';
 import {PetsCerca} from '../../components/petsCerca';
 import css from './index.module.css';
